95f6cc47041759cb66b4f9e3f2a205f5f280fe89
Dhonnbain <donovan.broquin@outlook.com>
2021-10-08 16:09:33 +0200

    docs: clean routes

--- index.php
+++ index.php
@@ -10,11 +10,16 @@ if ($_SERVER['REQUEST_URI'] === '/connexion') {
     require(__DIR__ . '/controleurs/connexionPageControleur.php');
 }
 
-// Traitement du formulaire de connexion
-if ($_SERVER['REQUEST_URI'] === '/connexion/traitement') {
-    require(__DIR__ . '/controleurs/connexionFormulaireControleur.php');
-}
-
+// Page d'administration
 if ($_SERVER['REQUEST_URI'] === '/admin') {
     require(__DIR__ . '/controleurs/adminPageControleur.php');
+}
+
+/***************************************************************
+ * Traitements formulaires
+ */
+
+// Connexion
+if ($_SERVER['REQUEST_URI'] === '/connexion/traitement') {
+    require(__DIR__ . '/controleurs/connexionFormulaireControleur.php');
 }
